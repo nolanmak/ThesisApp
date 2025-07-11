09a3e5471b304b033ea868f7a33c669e39172991
Nolan Makatche <nolanmak7@gmail.com>
2025-07-11 15:17:07 -0400

    lfg

--- src/components/Earnings/index.tsx
+++ src/components/Earnings/index.tsx
@@ -51,7 +51,7 @@ const Messages: React.FC = () => {
   } = useAudioWebSocket({
     onAudioNotification: (notification) => {
       console.log('Audio notification received:', notification);
-      // Refresh messages when audio notification receive
+      // Refresh messages when audio notification received
       refreshMessages();
     },
     onConnectionChange: (connected) => {
